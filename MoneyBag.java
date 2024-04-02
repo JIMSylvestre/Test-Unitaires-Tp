2fb5877f3c3aa79e9a6106d9d02be821b26aa66a
JIMSylvestre <149988071+JIMSylvestre@users.noreply.github.com>
2024-04-02 20:45:04 +0300

    Add files via upload

---
+++ MoneyBag.java
@@ -0,0 +1,92 @@
+package fr.emse.test;
+
+public class MoneyBag implements IMoney {
+    private Vector<Money> fMoneis = new Vector<Money>();
+    
+    public MoneyBag(Money m1, Money m2) {
+        appendMoney(m1);
+        appendMoney(m2);
+    }
+    
+    public MoneyBag(Money bag[]) {
+        for (int i = 0; i < bag.length; i++)
+            appendMoney(bag[i]);
+    }
+
+    
+    private void appendMoney(Money m) {
+        if (fMoneis.isEmpty()) {
+            fMoneis.add(m);
+        } else {
+            int i = 0;
+            while ((i < fMoneis.size()) && (!(fMoneis.get(i).currency().equals(m.currency()))))
+                i++;
+            if (i >= fMoneis.size()) {
+                fMoneis.add(m);
+            } else {
+                fMoneis.set(i, new Money(fMoneis.get(i).amount() + m.amount(), m.currency()));
+            }
+        }
+    }
+
+    @Override
+    public IMoney add(IMoney aMoney) {
+        return aMoney.addMoneyBag(this);
+    }
+
+    public IMoney addMoney(Money m) {
+        appendMoney(m);
+        return simplify();
+    }
+
+    public IMoney addMoneyBag(MoneyBag aMoneyBag) {
+        for (int i = 0; i < aMoneyBag.fMoneis.size(); i++)
+            appendMoney(aMoneyBag.fMoneis.get(i));
+        return simplify();
+    }
+    
+    private IMoney simplify() {
+        int nonZeroCount = 0;
+        Money nonZeroMoney = null;
+
+        for (Money money : fMoneis) {
+            if (money.amount() != 0) {
+                nonZeroCount++;
+                nonZeroMoney = money;
+            }
+        }
+
+        if (nonZeroCount == 1) {
+            return nonZeroMoney;
+        } else {
+            return this;
+        }
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true; // Check for same object
+      
+        if (o == null || getClass() != o.getClass()) return false; // Check for null or different class
+      
+        MoneyBag moneyBag = (MoneyBag) o; // Cast to MoneyBag
+
+        return fMoneis.equals(moneyBag.fMoneis); // Compare the contents of the bags
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("{");
+        for (int i = 0; i < fMoneis.size(); i++) {
+            if (i > 0) {
+                sb.append(", ");
+            }
+            sb.append(fMoneis.get(i));
+        }
+        sb.append("}");
+        return sb.toString();
+    }    
+}
+
---
+++ TestMoneyBag.java
@@ -0,0 +1,31 @@
+package fr.emse.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestMoneyBag {
+
+	@Before
+	public void setUp() {
+	Money f12CHF = new Money(12, "CHF");
+	Money f14CHF = new Money(14, "CHF");
+	Money f7USD = new Money( 7, "USD");
+	Money f21USD = new Money(21, "USD");
+	MoneyBag fMB1 = new MoneyBag(f12CHF, f7USD);
+	MoneyBag fMB2 = new MoneyBag(f14CHF, f21USD);
+	}
+	@Test
+	public void testBagEquals() {
+	Object fMB1 = null;
+	assertTrue(!fMB1.equals(null)); 
+	assertEquals(fMB1, fMB1); 
+	Object f12CHF = null;
+	assertTrue(!fMB1.equals(f12CHF)); 
+	assertTrue(!f12CHF.equals(fMB1)); 
+	Object fMB2 = null;
+	assertTrue(!fMB1.equals(fMB2));
+	}
+
+}
